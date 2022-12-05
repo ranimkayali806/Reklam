5eb381e145913d32fe0afbd7cf627c4940285012
Sven Widlund <sven@basengineering.se>
2022-12-05 11:01:06 +0100

    dddd

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class Employee(db.Model):
     birthdate = db.Column(db.DateTime,unique=False,nullable=False)
     city = db.Column(db.String(20),unique=False,nullable=True)
     shoesize = db.Column(db.Integer,unique=False,nullable=False)
-
+ 
    
 def CreateNew():
     b = Employee()
